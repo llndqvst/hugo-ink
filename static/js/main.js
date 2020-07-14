ec70ef934f0c7ee6217f56357bbb72defd3327cc
Lukas Lindqvist <lukas.lndqvst@gmail.com>
2020-07-15 01:33:34 +0200

    Change to use feather icons through svg instead of javascript

--- static/js/main.js
+++ static/js/main.js
@@ -30,16 +30,33 @@ document.addEventListener("DOMContentLoaded", function(){
   });
 });
 
+function createfeathericon(icon) {
+    var svgns = "http://www.w3.org/2000/svg";
+    var xlinkns = "http://www.w3.org/1999/xlink";
+
+    var svg = document.createElementNS(svgns, "svg");
+    svg.setAttribute("class", "feather")
+    var use = document.createElementNS(svgns, "use");
+    use.setAttributeNS(xlinkns, "href", icon);
+    svg.appendChild(use);
+    console.log(svg);
+    return svg;
+}
+
 function darkscheme(toggle, container) {
-  localStorage.setItem("scheme", "dark");
-  toggle.innerHTML = feather.icons.sun.toSvg();
-  toggle.className = "dark";
-  container.className = "dark";
+    localStorage.setItem("scheme", "dark");
+    // toggle.innerHTML = feather.icons.sun.toSvg();
+    toggle.innerHTML = "";
+    toggle.appendChild(createfeathericon("/icons/feather-sprite.svg#sun"));
+    toggle.className = "dark";
+    container.className = "dark";
 }
 
 function lightscheme(toggle, container) {
-  localStorage.setItem("scheme", "light");
-  toggle.innerHTML = feather.icons.moon.toSvg();
-  toggle.className = "light";
-  container.className = "";
+    localStorage.setItem("scheme", "light");
+    // toggle.innerHTML = feather.icons.moon.toSvg();
+    toggle.innerHTML = "";
+    toggle.appendChild(createfeathericon("/icons/feather-sprite.svg#moon"));
+    toggle.className = "light";
+    container.className = "";
 }
